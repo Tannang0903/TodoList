abe8701aeb4a08f78594fa3b65e8e63ea6ecb02f
= <=>
2023-02-14 22:22:46 +0700

    feat: validate form

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,9 @@ const contentEdit = document.getElementById("editContent");
 
 const btnAddTask = document.getElementById("btnAddTask");
 const btnCloseAddForm = document.getElementById("btnCloseAddForm");
-const formAddTask = document.querySelector(".form_add_new_todo");
+const formAddTask = document.querySelector(".form-add-new-todo");
 const btnCloseEditForm = document.getElementById("btnCloseEditForm");
-const formEditTask = document.querySelector(".form_edit_todo");
+const formEditTask = document.querySelector(".form-edit-todo");
 
 const TodoList = JSON.parse(localStorage.getItem("dataListTodo"));
 
@@ -85,8 +85,7 @@ function renderData(data) {
   countTask();
 }
 
-addForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+function insertTask() {
   data.push({
     type: "todo",
     category: categoryInput.value,
@@ -100,7 +99,7 @@ addForm.addEventListener("submit", (e) => {
   }
   localStorage.setItem("dataListTodo", JSON.stringify(data));
   renderData(data);
-});
+}
 
 function RemoveAt(index) {
   data.splice(index, 1);
@@ -178,3 +177,101 @@ function countTask() {
 }
 
 renderData(data);
+
+function Validator(options) {
+  var selectorRules = {};
+
+  function getParentElement(element, selector) {
+    while (element.parentElement) {
+      if (element.parentElement.matches(selector)) {
+        return element.parentElement;
+      }
+      element = element.parentElement;
+    }
+  }
+
+  function validate(inputElement, rule) {
+    const parentInputElement = getParentElement(
+      inputElement,
+      options.formGroupSelector
+    );
+    const errorElement = parentInputElement.querySelector(
+      options.errorSelector
+    );
+    const rules = selectorRules[rule.selector];
+    var errorMessage;
+
+    for (let i = 0; i < rules.length; i++) {
+      switch (inputElement.type) {
+        case "radio":
+          errorMessage = rules[i](
+            formElement.querySelector(rule.selector + ":checked")
+          );
+          break;
+        default:
+          errorMessage = rules[i](inputElement.value);
+      }
+      if (errorMessage) break;
+    }
+
+    if (errorMessage) {
+      errorElement.innerText = errorMessage;
+    } else {
+      errorElement.innerText = "";
+    }
+    return !errorMessage;
+  }
+
+  const formElement = document.querySelector(options.form);
+  if (formElement) {
+    formElement.onsubmit = function (e) {
+      e.preventDefault();
+      let isFormValid = true;
+      options.rules.forEach(function (rule) {
+        const inputElement = formElement.querySelector(rule.selector);
+        let isValid = validate(inputElement, rule);
+        if (!isValid) {
+          isFormValid = false;
+        }
+      });
+      if (isFormValid) {
+        insertTask();
+      }
+    };
+  }
+
+  options.rules.forEach(function (rule) {
+    if (Array.isArray(selectorRules[rule.selector])) {
+      selectorRules[rule.selector].push(rule.message);
+    } else {
+      selectorRules[rule.selector] = [rule.message];
+    }
+
+    var inputElements = formElement.querySelectorAll(rule.selector);
+    Array.from(inputElements).forEach(function (inputElement) {
+      inputElement.onblur = function () {
+        validate(inputElement, rule);
+      };
+
+      inputElement.oninput = function () {
+        var parentInputElement = getParentElement(
+          inputElement,
+          options.formGroupSelector
+        );
+        var errorElement = parentInputElement.querySelector(
+          options.errorSelector
+        );
+        errorElement.innerText = "";
+      };
+    });
+  });
+}
+
+Validator.isRequired = function (selector, message) {
+  return {
+    selector: selector,
+    message: function (value) {
+      return value ? undefined : message;
+    },
+  };
+};
